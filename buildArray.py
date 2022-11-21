611c9ac639a4039c778e0da90133ce66a8aba44b
Cypher Moon <temperaturecaleb@gmail.com>
2022-11-21 13:03:38 +0100

    created an array class

--- buildArray.py
+++ buildArray.py
@@ -2,25 +2,37 @@ class Array:
     count = 0
 
     def __init__(self, length):
-        self.arr = [] 
+        self.arr = [None] * length 
         self.length = length
     
     def __str__(self):
-        return f"{self.arr}"
+        return f"{self.arr[:self.count]}"
     
     def insert(self, element):
-        if self.count < self.length:
-            self.arr.append(element)
-            self.count += 1
-        else:
-            print("length is full")
+        if self.count >= self.length:
+            newArray = self.arr * 9
+            for item,i in enumerate(self.arr):
+                newArray[i] = item
+
+            self.arr = newArray
+            self.length = len(self.arr)
     
+        self.arr[self.count] = element
+        self.count += 1
+
+    def removeAt(self, index):
+        for i in range(index, self.length - 1):
+            self.arr[i] = self.arr[i + 1]
+
     def indexOf(self, element):
         return self.arr.index(element)
 
 
 arr = Array(1)
-arr.insert(3)
-arr.insert(6)
+arr.insert(2)
+arr.insert(1)
+arr.insert(5)
+arr.insert(18)
+arr.removeAt(1)
+
 print(arr)
-    
