4a8ee95c6e11897ee0c529fe92ec705030a103ae
Cypher Moon <temperaturecaleb@gmail.com>
2022-11-28 14:43:38 +0100

    implemented a custom linked list

---
+++ linked_list/LinkedList.java
@@ -0,0 +1,116 @@
+package linked_list;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node() {
+
+        }
+
+        public Node(int value) {
+            this.value = value;
+
+        }
+
+        @Override
+        public String toString() {
+            return Integer.toString(value);
+        }
+    }
+
+    private Node first;
+    private Node last;
+
+    public void addFirst(int value) {
+        Node node = new Node(value);
+
+        if (isEmpty()) {
+            first = last = node;
+        } else {
+            node.next = first;
+            first = node;
+        }
+
+    }
+
+    public void addLast(int value) {
+        Node node = new Node(value);
+
+        if (isEmpty())
+            first = last = node;
+        else {
+            this.last.next = node;
+            this.last = node;
+        }
+
+    }
+
+    public int indexOf(int value) {
+        int index = 0;
+        Node current = first;
+        while (current != null) {
+            if (current.value == value)
+                return index;
+
+            current = current.next;
+            index++;
+        }
+
+        index = -1;
+
+        return index;
+    }
+
+    public void deleteFirst() throws Exception {
+        if (isEmpty())
+            throw new NoSuchElementException("this element is not accessible");
+
+        if (first.next == null) {
+            first = last = null;
+            return;
+        }
+
+        Node temp = first.next;
+        first.next = null;
+        first = temp;
+    }
+
+    public void deleteLast() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last) {
+            first = last = null;
+            return;
+        }
+
+        Node previous = getPreviousNode(last);
+        last = previous;
+        last.next = null;
+
+    }
+
+    public Node getPreviousNode(Node node) {
+        Node current = first;
+
+        while (current != null) {
+            if (current.next == node)
+                return current;
+            current = current.next;
+        }
+
+        return null;
+    }
+
+    public boolean contains(int value) {
+        return indexOf(value) != -1;
+    }
+
+    private boolean isEmpty() {
+        return this.first == null;
+    }
+}
---
+++ linked_list/Main.java
@@ -0,0 +1,12 @@
+package linked_list;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        // linkedList.addFirst(1);
+        linkedList.addFirst(5);
+        linkedList.addFirst(10);
+        linkedList.addFirst(50);
+        linkedList.deleteLast();
+    }
+}
